33f272aacc30624015b7418ccfe86e6ac27b01e4
PokeADotProductions <zfitzpat@lion.lmu.edu>
2016-05-17 21:09:53 -0700

    Adding test functions

--- TicTacToe/TicTacToeTest.py
+++ TicTacToe/TicTacToeTest.py
@@ -1,13 +1,29 @@
 from TicTacToe import GameBoard
 
-gb = GameBoard()
+def test_is_valid_move():
+    print("Testing is_valid_move...")
+    gb1 = GameBoard()
+    gb1.place_piece([1,1])
+    if (not(gb1.is_valid_move([1,1]))):
+        print(" Success")
+    if (gb1.is_valid_move([1, 2])):
+        print(" Success")
+    if (not(gb1.is_valid_move([1,3]))):
+        print(" Success")
 
-print(gb.is_valid_move([1, 3]))
+def test_check_for_win():
+    print("\nTesting check_for_win...")
+    gb2 = GameBoard()
+    gb2.place_piece([0,0])
+    gb2.place_piece([1,0])
+    gb2.place_piece([1,1])
+    gb2.place_piece([2,0])
+    gb2.place_piece([2,2])
+    gb2.place_piece([2,1])
+    gb2.print_board()
 
-gb.place_piece([1,1])
+    if (gb2.check_for_win(gb2.pieces[gb2.turnNumber])):
+        print("Success")
 
-gb.place_piece([1,2])
-
-print(gb.turnNumber)
-
-gb.print_board()
+test_is_valid_move()
+test_check_for_win()
