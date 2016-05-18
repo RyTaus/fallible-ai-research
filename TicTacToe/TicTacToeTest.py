4d0fa6df19d67a22eb74b8e6e75cb2d10d70d399
PokeADotProductions <zfitzpat@lion.lmu.edu>
2016-05-17 21:24:35 -0700

    Made better variable names

--- TicTacToe/TicTacToeTest.py
+++ TicTacToe/TicTacToeTest.py
@@ -2,28 +2,26 @@ from TicTacToe import GameBoard
 
 def test_is_valid_move():
     print("Testing is_valid_move...")
-    gb1 = GameBoard()
-    gb1.place_piece([1,1])
-    if (not(gb1.is_valid_move([1,1]))):
+    gb = GameBoard()
+    gb.place_piece([1,1])
+    if (not(gb.is_valid_move([1,1]))):
         print(" Success")
-    if (gb1.is_valid_move([1, 2])):
+    if (gb.is_valid_move([1, 2])):
         print(" Success")
-    if (not(gb1.is_valid_move([1,3]))):
+    if (not(gb.is_valid_move([1,3]))):
         print(" Success")
 
-def test_check_for_win():
-    print("\nTesting check_for_win...")
-    gb2 = GameBoard()
-    gb2.place_piece([0,0])
-    gb2.place_piece([1,0])
-    gb2.place_piece([1,1])
-    gb2.place_piece([2,0])
-    gb2.place_piece([2,2])
-    gb2.place_piece([2,1])
-    gb2.print_board()
-
-    if (gb2.check_for_win(gb2.pieces[gb2.turnNumber])):
+def test_has_won():
+    print("\nTesting has_won...")
+    gb = GameBoard()
+    gb.place_piece([0,0])
+    gb.place_piece([1,0])
+    gb.place_piece([1,1])
+    gb.place_piece([2,0])
+    gb.place_piece([2,2])
+    gb.place_piece([2,1])
+    if (gb.has_won(gb.pieces[gb.turnNumber])):
         print("Success")
 
 test_is_valid_move()
-test_check_for_win()
+test_has_won()
