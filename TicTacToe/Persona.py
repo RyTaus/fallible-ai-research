5568830ffd13f46d4812c297bcffdf615d4c9683
RyTaus <rtaus1@lion.lmu.edu>
2016-05-18 17:38:24 -0700

    fixed reading files and began looking at past results

--- TicTacToe/Persona.py
+++ TicTacToe/Persona.py
@@ -7,19 +7,23 @@ class Persona:
     # this is where end of game / memory file stuff is
     def read_history(self, fileName):
         mem = dict()
-        with open(fileName, "a+") as ins:
+        with open(fileName, "r") as file:
             split = []
-            for line in ins:
+            # print(file.read())
+            for line in file.readlines():
+                # print(line)
                 split = line.split(" ")
-                mem[split[0]] = (float(mem[1]), float(mem[2]))
-        ins.close()
+                split[2] = split[2][:-1]
+                print(split)
+                mem[split[0]] = (float(split[1]), float(split[2]))
+        file.close()
+        print(mem)
         return mem
 
 
     def write_history(self, fileName):
         file = open(fileName, "w")
         for brd in self.memory:
-            print(brd)
             file.write(brd)
             file.write(" ")
             file.write(str(self.memory[brd][0]))
@@ -37,13 +41,28 @@ class Persona:
     # this is where in game memory is
     def add_board(self, brdStrng):
         if brdStrng not in self.boardStates:
-            print(brdStrng)
             self.boardStates.append(brdStrng)
 
 
-    def __init__(self):
+    def __init__(self, t):
         self.memory = self.read_history("history.txt")
+        self.currBoard = GameBoard()
         self.boardStates = [GameBoard().to_string_one_line()]
+        self.piece = t
+
+    # default operations of a "player"
+
+    # def evaluate_options():
+    #     UNRESEARCHEDBOARDVALUE = .5
+    #     currWinner = None
+    #     editBoard = GameBoard()
+    #     editBoard.board = self.currBoard.board
+    #     for i in range(3):
+    #         for j in range(3):
+    #             if (self.currBoard.is_valid_move((i, j))):
+    #                 editBoard.make_move((i, j))
+
+
 
 
     def make_move(self):
@@ -54,10 +73,13 @@ class Persona:
 
     def update(self, board, pos):
         self.add_board(board.to_string_one_line())
-        self.currBoard = board
+        # if (board.to_string_one_line() != self.currBoard.to_string_one_line()):
+        self.currBoard.board = board.board
 
     def end_game(self, board, w):
-        print(self.boardStates)
+        TIEWORTH = .5
+        d = {"-": TIEWORTH, "O": 1 if self.piece == w else 0, "X": 1 if self.piece == w else 0}
+
         self.add_board(board.to_string_one_line())
-        [self.add_board_to_memory(b, w) for b in self.boardStates]
+        [self.add_board_to_memory(b, d[w]) for b in self.boardStates]
         self.write_history("history.txt")
