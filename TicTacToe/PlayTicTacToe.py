ca488c630fc8ee858ddd7311725ee4126e738d1c
PokeADotProductions <zfitzpat@lion.lmu.edu>
2016-05-18 18:40:15 -0700

    Update so a person or AI (persona) could play

--- TicTacToe/PlayTicTacToe.py
+++ TicTacToe/PlayTicTacToe.py
@@ -1,26 +1,41 @@
 from Board import GameBoard
+from Persona import Persona
+from Player import Player
 
 def play_tic_tac_toe():
 
     gb = GameBoard()
-    coord = [3,3]
+    player = ["", ""]
+    player[0] = input("Is O a person or a persona? ")
+    player[1] = input("Is X a person or a persona? ")
+
+    activePlayer = [None, None]
+
+    if player[0] == "persona":
+        activePlayer[0] = Persona("O")
+    else:
+        activePlayer[0] = Player("O")
+
+    if player[1] == "persona":
+        activePlayer[1] = Persona("X")
+    else:
+        activePlayer[1] = Player("X")
+
 
     while (not gb.has_won(gb.pieces[gb.turnNumber]) and not gb.is_tie()):
         gb.switch_turn()
         gb.print_board()
+        print("\n")
 
-        while not gb.is_valid_move(coord):
-            xCoord = input("%ss please input an x coordinate " %gb.pieces[gb.turnNumber])
-            yCoord = input("%ss please input an y coordinate " %gb.pieces[gb.turnNumber])
-            xCoord = int(xCoord)
-            yCoord = int(yCoord)
-            coord = [xCoord, yCoord]
-            if not gb.is_valid_move(coord):
-                print("%ss did not input a valid move, please input another coordinate..." %gb.pieces[gb.turnNumber])
+        if(player[gb.turnNumber] == "persona"):
+            coord = activePlayer[gb.turnNumber].make_move()
+        else:
+            coord = activePlayer[gb.turnNumber].make_move(gb)
 
         gb.place_piece(coord)
 
     gb.print_board()
+
     if gb.is_tie():
         print("Xs and Os Tied!")
     else:
