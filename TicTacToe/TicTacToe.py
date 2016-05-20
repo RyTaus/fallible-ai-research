ee95578a2af56d2d98088d088451457ca8315ac7
RyTaus <rtaus1@lion.lmu.edu>
2016-05-20 13:04:18 -0700

    Update TicTacToe.py

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,4 +1,7 @@
 from Board import GameBoard
+from Player import Player
+from Players import Persona
+import random
 import pygame
 
 pygame.init()
@@ -20,11 +23,20 @@ clock = pygame.time.Clock()
 
 cellStates = ["-", "O", "X"]
 colors = [WHITE, RED, BLUE]
+
+
 gb = GameBoard()
+p1 = Player("O")
+p2 = Persona("X", learning = False)
+players = [p1, p2]
+
+
 
 def get_cell(msx, msy):
     return (msx // 200, msy // 200)
 
+
+count = 0
 # -------- Game Loop -----------
 while not done:
     # --- Main event loop
@@ -32,27 +44,35 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
- 
+    
     # --- Game logic
+
     mx = pygame.mouse.get_pos()[0]
     my = pygame.mouse.get_pos()[1]
     mbd = pygame.mouse.get_pressed()[0]
     # print(get_cell(mx, my))
     loc = get_cell(mx, my)
+    p1.update(gb, loc)
+    turn = gb.turnNumber
 
 
 
     # print(gb.turnNumber)
-    if mbd:
-        if gb.is_valid_move(loc):
-            print(loc)
-            gb.place_piece(loc)
+    if mbd and not mbdPrev or False:
+        if gb.is_valid_move(players[turn].make_move(gb)):
+            gb.place_piece(players[turn].make_move(gb))
 
-            if gb.has_won(gb.pieces[gb.turnNumber]):
-                print("WINNER")
+            results = gb.get_results()
+            if results != None:
+                print(results)
+                # p2.end_game(gb, results)
+                # p1.end_game(gb, results)
                 gb = GameBoard()
-
-            gb.switch_turn()
+                gb.turnNumber = random.choice([0, 1])
+                if count == 1000:
+                    done = True
+            else:
+                gb.switch_turn()
 
 
  
@@ -65,6 +85,8 @@ while not done:
         for j in range(3):
                 pygame.draw.rect(screen, colors[cellStates.index(gb.board[i][j])], [i * SQUARE_SIZE, j * SQUARE_SIZE, 200, 200], 0)
 
+    
+    mbdPrev = mbd
     # pygame.draw.rect(screen, RED, [i * SQUARE_SIZE, j * SQUARE_SIZE, 200, 200], 0)
  
  
@@ -72,4 +94,4 @@ while not done:
     pygame.display.update()
  
     # --- Frames Per Second
-    clock.tick(60)
+    clock.tick(600)
