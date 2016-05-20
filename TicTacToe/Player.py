f833ea4454fb7e86be3fce1c71098370b9ccf66f
RyTaus <rtaus1@lion.lmu.edu>
2016-05-20 13:05:24 -0700

    player for mouse input game

--- TicTacToe/Player.py
+++ TicTacToe/Player.py
@@ -1,21 +1,15 @@
-from Board import GameBoard
-
 class Player:
+	def __init__(self, t):
+		self.x = 0
+		self.y = 0
+		self.piece = t
 
-    def __init__(self, t):
-        self.piece = t
+	def make_move(self, board):
+		return (self.x, self.y)
 
-    def make_move(self, gameBoard):
-        coord = [3,3]
-        gb = gameBoard
-        
-        while not gb.is_valid_move(coord):
-            xCoord = input("%ss please input an x coordinate " %gb.pieces[gb.turnNumber])
-            yCoord = input("%ss please input an y coordinate " %gb.pieces[gb.turnNumber])
-            xCoord = int(xCoord)
-            yCoord = int(yCoord)
-            coord = [xCoord, yCoord]
-            if not gb.is_valid_move(coord):
-                print("%ss did not input a valid move, please input another coordinate..." %gb.pieces[gb.turnNumber])
+	def update(self, board, pos):
+		self.x = pos[0]
+		self.y = pos[1]
 
-        return coord
+	def end_game(self, b, r):
+		return None
