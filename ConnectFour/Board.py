7181f0aa2283d4c2c0f5d54f79dfd88c6d516e44
RyTaus <rtaus1@lion.lmu.edu>
2016-05-25 16:19:28 -0700

    added checks for getting amount of blocks

--- ConnectFour/Board.py
+++ ConnectFour/Board.py
@@ -11,8 +11,10 @@ class GameBoard:
         self.turnNumber = 0
 
 
-    def switch_turn(self):
+    def increment_turn(self):
         self.turnNumber += 1
+
+    def switch_turn(self):
         self.turnState = (self.turnState + 1) % 2
 
     def print_board(self):
@@ -122,6 +124,10 @@ class GameBoard:
 
     def copy(self):
         copy = GameBoard()
+        copy.turnNumber = self.turnNumber
+        copy.pieces = self.pieces
+        copy.turnState = self.turnState
+        copy.lastPosition = self.lastPosition
         for i in range(self.height):
             for j in range(self.width):
                 copy.board[j][i] = self.board[j][i]
@@ -134,3 +140,110 @@ class GameBoard:
             return "-"
 
         return None
+
+    def will_win_on_col(self, col, piece):
+        boardCopy = self.copy()
+        if boardCopy.is_valid_action(col):
+            boardCopy.place_piece(col)
+            boardCopy.board[boardCopy.lastPosition[0]][boardCopy.lastPosition[1]] = piece
+        return boardCopy.has_won(piece)
+
+
+    def cocks_to_block(self, piece):
+        cocksToBlock = [False for i in range(7)]
+        for i in range(7):
+            cocksToBlock[i] = (self.will_win_on_col(i, piece))
+        return cocksToBlock
+
+    def check_for_trap(self, piece):
+        otherPiece = {"X" : "O", "O" : "X"}
+        opponentWins = [False for i in range(7)]
+        for i in range(7):
+            copy = self.copy()
+            if copy.is_valid_action(i):
+                copy.place_piece(i)
+            opponentWins[i] = copy.will_win_on_col(i, otherPiece[copy.pieces[copy.turnState]])
+        return opponentWins
+
+
+    def in_bounds(self, loc):
+        return loc[0] < 0 or loc[0] >= self.width or loc[1] < 0 or loc[1] >= self.height
+
+    def get_value(self, d, init, piece):
+        dic = {"X" : "O", "O" : "X"}
+        amount = 0
+        types = [(), (2, -1, -2), (3, 2, 1, -1), ()]
+        for i in range(1, 4):
+            x = init[0] + (i * d[0])
+            y = init[1] + (i * d[1])
+            if self.in_bounds((x, y)):
+                break
+            p = self.board[x][y]
+            # print(p)
+            if  p == piece:
+                amount += 1
+            elif p != "-":
+                break
+        matters = False
+
+        l = types[amount]
+        for v in l:
+            x = init[0] + (v * d[0])
+            y = init[1] + (v * d[1])
+            if (x < 0 or x >= self.width) or (y < 0 or y >= self.height):
+                pass
+            elif (self.board[x][y] != dic[piece]):
+                matters = True
+            else:
+                pass
+        if (amount == 2 and matters):
+            check = [-1, 1, 2]
+            if self.in_bounds((init[0] + (-1 * d[0]), init[1] + (-1 * d[1]))) and self.in_bounds((init[0] + (2 * d[0]), init[1] + (2 * d[1]))):
+                if sum([1 if board[init[0] + (v * d[0])][init[1] + (v * d[1])] == piece else 0 for v in check]) == 3:
+                    return 3
+        if (amount == 1 and matters):
+            check = [-2, -1, 1]
+            if self.in_bounds((init[0] + (-2 * d[0]), init[1] + (-2 * d[1]))) and self.in_bounds((init[0] + (1 * d[0]), init[1] + (1 * d[1]))):
+                if sum([1 if board[init[0] + (v * d[0])][init[1] + (v * d[1])] == piece else 0 for v in check]) == 3:
+                    return 3
+
+        if amount == 3:
+            return 3
+
+        if matters:
+            return amount
+        return 0
+
+    def rate_move(self, col, piece): # maybe add in a weight of time till reachable
+        d = {"X" : "O", "O" : "X"}
+        values = [0 for i in range(7)]
+        values[0] = (5 - abs(3 - col)) / 3
+        init = (col, self.height - self.board[col].count("-"))
+
+        l = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+        for i in range(len(l)): 
+            v = self.get_value(l[i], init, d[piece])
+            if v:
+                values[v] += 1        # find blocks
+            v = self.get_value(l[i], init, piece)
+            # print("block going :" + str(l[i]) + " = " + str(v))
+            if v:
+                values[v + 3] += 1           # find connections
+        # print(values)
+        return values
+
+# b = GameBoard()
+# b.place_piece(2)
+# b.place_piece(2)
+# b.place_piece(2)
+# b.switch_turn()
+# b.place_piece(4)
+# b.place_piece(4)
+# b.place_piece(4)
+# b.print_board()
+
+# b.rate_move(2, "O")
+# b.place_piece(2)
+# b.print_board()
+
+# print(b.lastPosition)
