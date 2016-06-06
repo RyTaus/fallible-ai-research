60a56bc64a48a75fe0613354d399d70fdbfc2c67
RyTaus <rtaus1@lion.lmu.edu>
2016-06-05 20:30:41 -0700

    Create Coord.java

---
+++ DoH/Coord.java
@@ -0,0 +1,26 @@
+
+public class Coord {
+	
+	public int x;
+	public int y;
+	
+	public Coord(int ex, int why) {
+		x = ex;
+		y = why;
+	}
+	
+	public void move(Coord c) {
+		x += c.x;
+		y += c.y;
+	}
+	
+	public void moveTo(Coord c) {
+		x = c.x;
+		y = c.y;
+	}
+	
+	public int distance(Coord c) {
+		return Math.abs(c.x - x) + Math.abs(c.y - y);
+	}
+
+}
