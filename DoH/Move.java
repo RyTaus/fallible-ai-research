2dfa79ea790f1df238c29c67c4f58a3e41cef799
PokeADotProductions <zfitzpat@lion.lmu.edu>
2016-06-07 21:19:08 -0700

    Added moveScore

--- DoH/Move.java
+++ DoH/Move.java
@@ -2,14 +2,17 @@ public class Move {
 
     public Unit troop;
     public Unit target;
+    public int moveScore;
     public Coord destination;
     public ActionMenu.Option action;
+
     
-    public Move(Unit person, Unit enemy, Coord des, ActionMenu.Option choice) {
+    public Move(Unit person, Unit enemy, Coord des, ActionMenu.Option choice, int score) {
         troop = person;
         target = enemy;
         destination = des;
         action = choice;
+        moveScore = score;
     }
 
 }
