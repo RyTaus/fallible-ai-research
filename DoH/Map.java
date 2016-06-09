784af3a131c07fccfa3bc4c39654f7fb35516bd9
PokeADotProductions <zfitzpat@lion.lmu.edu>
2016-06-08 21:08:40 -0700

    Added checks for a screen's topLeft

--- DoH/Map.java
+++ DoH/Map.java
@@ -8,82 +8,108 @@ import java.nio.charset.Charset;
 
 
 public class Map {
-	
-	public int width;
-	public int height;
-	public Cell[][] map;
-	
-	public Coord cursor;
-	public Coord marked;
-	
-	public Map(String fileName) {
-		map = readFile(fileName);
-		width = map.length;
-		height = map[0].length;
-		cursor = new Coord(2, 4);
-		marked = new Coord(2, 4);
-	}
-	
-	public Cell[][] readFile(String s) {
-		try {
-			int width = 0;
-			int row = 0;
-			char[][] charArray = new char[256][256];
+    
+    public int width;
+    public int height;
+    public Cell[][] map;
+    
+    public Coord cursor;
+    public Coord marked;
 
-			InputStream is;
-			is = new FileInputStream(s);
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-			
-			String line;
-			while ((line = br.readLine()) != null) {
-		        String[] nums = line.split("");
-		        if (nums.length > width) {
-		        	width = nums.length;
-		        }
-		        for (int col = 0; col < nums.length; col++) {
-		            char n = nums[col].charAt(0);
-		            charArray[col][row] = n;  
-		        }
-		        row++;
-		    }
-		    
-			Cell[][] m = new Cell[width][row];
-			for (int col = 0; col < width; col++) {
-				for (int row1 = 0; row1 < row; row1++) {
-		            m[col][row1] = new Cell(charArray[col][row1]);
-				}
-			}
-			br.close();
-			return m;
-		} catch (FileNotFoundException fnfe) {
-			System.out.println("fnfe");
-		} catch (IOException ioe) {
-			System.out.println("ioe");
-		}
-		System.out.println("NOOOOOO");
-		return null;
-	}
-	
-	public Cell getCell(Coord c) {
-		return map[c.x][c.y];
-	}
+    public Coord topLeft;
+    public int SCREEN_WIDTH;
+    public int SCREEN_HEIGHT;
+    public int MARGIN;
+    
+    public Map(String fileName) {
+        map = readFile(fileName);
+        width = map.length;
+        height = map[0].length;
+        cursor = new Coord(2, 4);
+        marked = new Coord(2, 4);
+        topLeft = new Coord(2,2);
+        SCREEN_WIDTH = 3;
+        SCREEN_HEIGHT = 3;
+        MARGIN = 1;
+    }
 
-	public void moveCursor(Direction dir) {
-		Coord c = dir.toCoord();
-		int newX = cursor.x + c.x;
-		int newY = cursor.y + c.y;
-		
-		if (newX < 0 || newX > width - 1) {
-			newX = cursor.x;
-		}
-		
-		if (newY < 0 || newY > height - 1) {
-			newY = cursor.y;
-		}
-		Coord nc = new Coord(newX, newY);
-		cursor.moveTo(nc);
-	}
-	
+    public Coord getRelativeCoord(Coord c) {
+        return c.getDirection(topLeft);
+    }
+    
+    public Cell[][] readFile(String s) {
+        try {
+            int width = 0;
+            int row = 0;
+            char[][] charArray = new char[256][256];
 
+            InputStream is;
+            is = new FileInputStream(s);
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] nums = line.split("");
+                if (nums.length > width) {
+                    width = nums.length;
+                }
+                for (int col = 0; col < nums.length; col++) {
+                    char n = nums[col].charAt(0);
+                    charArray[col][row] = n;  
+                }
+                row++;
+            }
+            
+            Cell[][] m = new Cell[width][row];
+            for (int col = 0; col < width; col++) {
+                for (int row1 = 0; row1 < row; row1++) {
+                    m[col][row1] = new Cell(charArray[col][row1]);
+                }
+            }
+            br.close();
+            return m;
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("fnfe");
+        } catch (IOException ioe) {
+            System.out.println("ioe");
+        }
+        System.out.println("NOOOOOO");
+        return null;
+    }
+    
+    public Cell getCell(Coord c) {
+        return map[c.x][c.y];
+    }
+
+    public void moveCursor(Direction dir) {
+        Coord c = dir.toCoord();
+
+        int newX = cursor.x + c.x;
+        int newY = cursor.y + c.y;
+        
+        if (newX < 0 || newX > width - 1) {
+            newX = cursor.x;
+        }
+        
+        if (newY < 0 || newY > height - 1) {
+            newY = cursor.y;
+        }
+
+        Coord nc = new Coord(newX, newY);
+        Coord scrCurs = getRelativeCoord(nc);
+
+        if (scrCurs.y >= SCREEN_HEIGHT && (topLeft.y + SCREEN_HEIGHT) <= height) {
+            topLeft.y += 1;
+        } else if (scrCurs.x >= SCREEN_WIDTH && (topLeft.x + SCREEN_WIDTH) <= width) {
+            topLeft.x += 1;
+        } else if (scrCurs.y < topLeft.y && (topLeft.y - 1) <= 0) {
+            topLeft.y -= 1;
+        } else if (scrCurs.x < topLeft.x && (topLeft.x - 1) <= 0) {
+            topLeft.y -= 1;
+        }
+
+        cursor.moveTo(nc);
+    }
+    
 }
