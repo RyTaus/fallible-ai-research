6f7dd384067954aa156282a15acc60c17c98ec0e
PokeADotProductions <zfitzpat@lion.lmu.edu>
2016-06-18 21:07:24 -0700

    Added a few levels

--- DoH/Levels.java
+++ DoH/Levels.java
@@ -81,47 +81,96 @@ public class Levels {
     }
     
     public GameBoard getLevel(int i) {
-    	switch (i) {
-    	case 1:
-    		Team player = new Team(new Unit[]{new Unit(new Coord(1, 2), 3, 20, 6, 3, 5, Class.Gunner, TeamType.PLAYER),
-    				new Unit( new Coord(3, 1), 3, 20, 7, 3, 6, Class.Gunner, TeamType.PLAYER)}, TeamType.PLAYER);
-    		Team enemy = new Team(new Unit[]{new Unit(new Coord(1, 1), 3, 20, 6, 1, 5, Class.Swordsman, TeamType.ENEMY),
-    				new Unit( new Coord(2, 2), 3, 4, 2, 6, 3, Class.Swordsman, TeamType.ENEMY),
-    				new Unit( new Coord(4, 4), 3, 20, 7, 4, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
-    		return new GameBoard(new Map("src/Level1"), player, enemy, new HumanPlayer(), new EnemyPlayer());
-    	case 2:
-    		player = new Team(new Unit[]{new Unit(new Coord(1, 2), 3, 20, 6, 3, 5, Class.Gunner, TeamType.PLAYER),
-    				new Unit( new Coord(3, 1), 3, 20, 7, 3, 6, Class.Gunner, TeamType.PLAYER),
-    				new Unit( new Coord(3, 3), 3, 20, 7, 3, 6, Class.Swordsman, TeamType.PLAYER)}, TeamType.PLAYER);
-    		enemy = new Team(new Unit[]{new Unit(new Coord(1, 1), 3, 20, 6, 1, 5, Class.Swordsman, TeamType.ENEMY),
-    				new Unit( new Coord(2, 2), 3, 4, 2, 6, 3, Class.Swordsman, TeamType.ENEMY),
-    				new Unit( new Coord(4, 4), 3, 20, 7, 4, 5, Class.Swordsman, TeamType.ENEMY),
-    				new Unit( new Coord(7, 7), 3, 20, 7, 4, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
-    		return new GameBoard(new Map("src/Level1"), player, enemy, new HumanPlayer(), new EnemyPlayer());
-    	case 0:
-    		player = new Team(new Unit[]{new Unit(new Coord(3, 2), 3, 20, 6, 3, 5, Class.Gunner, TeamType.PLAYER)}, TeamType.PLAYER);
-    		enemy = new Team(new Unit[]{new Unit(new Coord(8, 8), 3, 20, 6, 3, 5, Class.Gunner, TeamType.ENEMY)}, TeamType.ENEMY);
-    		return new GameBoard(new Map("src/test"), player, enemy, new HumanPlayer(), new EnemyPlayer());
-    	case 3:
-    		player = new Team(new Unit[]{new Unit(new Coord(1, 1), 3, 20, 8, 4, 5, Class.Swordsman, TeamType.PLAYER),
-    				new Unit(new Coord(0, 0), 3, 17, 5, 2, 33, Class.Gunner, TeamType.PLAYER)}, TeamType.PLAYER);
-    		enemy = new Team(new Unit[]{new Unit(new Coord(3, 1), 6, 20, 6, 3, 5, Class.Swordsman, TeamType.ENEMY),
-    				new Unit(new Coord(4, 5), 9, 20, 6, 3, 5, Class.Swordsman, TeamType.ENEMY),
-    				new Unit(new Coord(6, 1), 12, 24, 7, 3, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
-    		return new GameBoard(new Map("src/Level2"), player, enemy, new HumanPlayer(), new EnemyPlayer());
-    	case 4:
-    		player = new Team(new Unit[]{new Unit(new Coord(0, 1), 3, 20, 8, 4, 5, Class.Swordsman, TeamType.PLAYER),}, TeamType.PLAYER);
-    		enemy = new Team(new Unit[]{new Unit(new Coord(2, 1), 6, 30, 6, 9, 5, Class.Swordsman, TeamType.ENEMY),
-    				new Unit(new Coord(2, 2), 12, 24, 7, 1, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
-    		return new GameBoard(new Map("src/blocked"), player, enemy, new HumanPlayer(), new EnemyPlayer());
-    	}
-    	
-    	return null;
+        switch (i) {
+        case 1:
+            Team player = new Team(new Unit[]{new Unit(new Coord(1, 2), 3, 20, 6, 3, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit( new Coord(3, 1), 3, 20, 7, 3, 6, Class.Gunner, TeamType.PLAYER)}, TeamType.PLAYER);
+            Team enemy = new Team(new Unit[]{new Unit(new Coord(1, 1), 3, 20, 6, 1, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit( new Coord(2, 2), 3, 4, 2, 6, 3, Class.Swordsman, TeamType.ENEMY),
+                    new Unit( new Coord(4, 4), 3, 20, 7, 4, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/Level1"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 2:
+            player = new Team(new Unit[]{new Unit(new Coord(1, 2), 3, 20, 6, 3, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit( new Coord(3, 1), 3, 20, 7, 3, 6, Class.Gunner, TeamType.PLAYER),
+                    new Unit( new Coord(3, 3), 3, 20, 7, 3, 6, Class.Swordsman, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{new Unit(new Coord(1, 1), 3, 20, 6, 1, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit( new Coord(2, 2), 3, 4, 2, 6, 3, Class.Swordsman, TeamType.ENEMY),
+                    new Unit( new Coord(4, 4), 3, 20, 7, 4, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit( new Coord(7, 7), 3, 20, 7, 4, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/Level1"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 0:
+            player = new Team(new Unit[]{new Unit(new Coord(3, 2), 3, 20, 6, 3, 5, Class.Gunner, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{new Unit(new Coord(8, 8), 3, 20, 6, 3, 5, Class.Gunner, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/test"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 3:
+            player = new Team(new Unit[]{new Unit(new Coord(1, 1), 3, 20, 8, 4, 5, Class.Swordsman, TeamType.PLAYER),
+                    new Unit(new Coord(0, 0), 3, 17, 5, 2, 33, Class.Gunner, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{new Unit(new Coord(3, 1), 6, 20, 6, 3, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(4, 5), 9, 20, 6, 3, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(6, 1), 12, 24, 7, 3, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/Level2"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 4:
+            player = new Team(new Unit[]{new Unit(new Coord(0, 1), 3, 20, 8, 4, 5, Class.Swordsman, TeamType.PLAYER),}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{new Unit(new Coord(2, 1), 6, 30, 6, 9, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(2, 2), 12, 24, 7, 1, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/blocked"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 5:
+            player = new Team (new Unit[]{
+                    new Unit(new Coord(4, 0), 4, 20, 10, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(5, 0), 4, 20, 12, 7, 5, Class.Swordsman, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{
+                    new Unit(new Coord(7, 3), 3, 20, 8, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(2, 0), 3, 20, 9, 4, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(0,2), 3, 20, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(0, 4), 3, 25, 12, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(1, 4), 3, 20, 10, 4, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/test8"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 6:
+            player = new Team (new Unit[]{
+                    new Unit(new Coord(1, 0), 4, 20, 10, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(3, 0), 4, 20, 10, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(2, 0), 4, 20, 10, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(0, 0), 4, 20, 12, 5, 5, Class.Swordsman, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{
+                    new Unit(new Coord(0, 3), 4, 20, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(1, 4), 4, 20, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(5, 7), 4, 20, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(1, 5), 4, 20, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(0, 7), 3, 25, 12, 5, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/test9"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 7:
+            player = new Team (new Unit[]{
+                    new Unit(new Coord(4, 0), 4, 16, 10, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(1, 0), 4, 16, 10, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(5, 0), 4, 20, 12, 5, 5, Class.Swordsman, TeamType.PLAYER),
+                    new Unit(new Coord(0, 0), 4, 20, 12, 5, 5, Class.Swordsman, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{
+                    new Unit(new Coord(0, 3), 4, 14, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(5, 2), 4, 14, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(5, 5), 4, 14, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(5, 3), 4, 18, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(4, 5), 4, 18, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(6, 5), 3, 22, 12, 6, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/test10"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        case 8: 
+            player = new Team (new Unit[]{
+                    new Unit(new Coord(0, 0), 4, 10, 18, 4, 5, Class.Gunner, TeamType.PLAYER),
+                    new Unit(new Coord(1, 1), 4, 20, 12, 5, 5, Class.Swordsman, TeamType.PLAYER)}, TeamType.PLAYER);
+            enemy = new Team(new Unit[]{
+                    new Unit(new Coord(9, 3), 4, 14, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(7, 3), 4, 14, 10, 4, 5, Class.Gunner, TeamType.ENEMY),
+                    new Unit(new Coord(10, 2), 4, 18, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(3, 3), 4, 18, 10, 5, 5, Class.Swordsman, TeamType.ENEMY),
+                    new Unit(new Coord(3, 0), 3, 20, 12, 3, 5, Class.Swordsman, TeamType.ENEMY)}, TeamType.ENEMY);
+            return new GameBoard(new Map("src/test11"), player, enemy, new HumanPlayer(), new EnemyPlayer());
+        }
+        
+        return null;
     }
     
     public GameBoard getTestLevel(int i) {
-    	Global.screenHeight = testBoards.get(i).map.height;
-    	Global.screenWidth = testBoards.get(i).map.width;
+        Global.screenHeight = testBoards.get(i).map.height;
+        Global.screenWidth = testBoards.get(i).map.width;
         return testBoards.get(i);
     }
 }
